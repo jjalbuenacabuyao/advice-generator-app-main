53d42b7b3e9d1458bc31a3a3f4ea1d3e3897e136
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-13 17:50:10 +0800

    added custom colors and font-family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,18 @@ module.exports = {
     "./public/index.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "manrope" : ["Manrope", "sans-serif"],
+      },
+      colors: {
+        "pr-light-cyan": "hsl(193, 38%, 86%)",
+        "pr-neon-green": "hsl(150, 100%, 66%)",
+        "nt-grayish-blue": "hsl(217, 19%, 38%)",
+        "nt-dark-grayish-blue": "hsl(217, 19%, 24%)",
+        "nt-dark-blue": "hsl(218, 23%, 16%)"
+      }
+    },
   },
   plugins: [],
 }
